cfc80451c4a2ec053c6cae819f1a5c00c0289d09
Arvid <arvid.rydh@gmail.com>
2023-04-14 22:47:15 +0200

    fixed start page, wish i had self discipline

--- src/routes/Welcome.jsx
+++ src/routes/Welcome.jsx
@@ -1,11 +1,15 @@
 import { Link } from "react-router-dom";
-
+import './welcome.css';
+import dogDayCare from '../assets/dogs.jpg';
 
 const Welcome = () => {
 
     return(
         <body className="welcomeBackground">
             <h2>Välkommen till Doggy Daycare!</h2>
+            <div>
+                <img src={dogDayCare}/>
+            </div>
             <Link to='/catalogue'>
                 <button>Visa registrerade hundar</button>
             </Link>
